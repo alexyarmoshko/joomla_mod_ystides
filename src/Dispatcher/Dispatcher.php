3e32de5f3884af58ea97a5d7d080f19f7d2a9792
Alex Yarmoshko <48326842+alexyarmoshko@users.noreply.github.com>
2026-01-02 17:52:04 +0000

    Initial module version 1.0.1

---
+++ src/Dispatcher/Dispatcher.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_ystides
+ *
+ * @copyright   (C) 2025 YSTides
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Module\Ystides\Site\Dispatcher;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Dispatcher class for mod_ystides.
+ *
+ * @since  1.0.0
+ */
+class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    /**
+     * Returns the layout data.
+     *
+     * @return  array
+     *
+     * @since   1.0.0
+     */
+    protected function getLayoutData(): array
+    {
+        $data = parent::getLayoutData();
+
+        $helper = $this->getHelperFactory()->getHelper('YstidesHelper');
+        $data   = array_merge($data, $helper->getLayoutVariables($data['params']));
+
+        return $data;
+    }
+}
